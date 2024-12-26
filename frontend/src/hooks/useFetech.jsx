ecd76dc51b7e717d9114cd7c9554b3595dea6b55
Priyanka <priyankashah3107@gmail.com>
2024-12-26 19:08:36 +0530

    feat: workingAllCourseCompo

---
+++ frontend/src/hooks/useFetech.jsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!url) return;
+
+    const source = axios.CancelToken.source();
+    const getData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await axios.get(url, { cancelToken: source.token });
+        setData(response.data);
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          console.log("Request Canceled:", error.message);
+        } else {
+          setError(error.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getData();
+
+    return () => {
+      source.cancel("Operation canceled by the user.");
+    };
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
